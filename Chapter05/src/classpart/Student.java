4887d201858ef9a219ab66cce610ade378df5cf3
junstone1995 <boris0730@gmail.com>
2020-08-07 15:42:36 +0900

    class

---
+++ Chapter05/src/classpart/Student.java
@@ -0,0 +1,12 @@
+package classpart;
+
+public class Student {
+	
+	int studentID;
+	String studentName;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+}

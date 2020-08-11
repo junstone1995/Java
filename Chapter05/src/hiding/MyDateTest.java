b0f666a1be0298486c325ef8c01de2fbd6a9f1f7
junstone1995 <boris0730@gmail.com>
2020-08-11 22:55:54 +0900

    hiding

---
+++ Chapter05/src/hiding/MyDateTest.java
@@ -0,0 +1,13 @@
+package hiding;
+
+public class MyDateTest {
+	public static void main(String[] args) {
+		MyDate date = new MyDate();
+		
+		date.setYear(2020);
+		date.setMonth(7);
+		date.setDay(30);
+		
+		date.showDate();
+	}
+}

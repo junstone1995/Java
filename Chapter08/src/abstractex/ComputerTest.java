cd3b4bbef905e05cb8d309ffee193bdf097ba7d9
junstone1995 <boris0730@gmail.com>
2020-08-19 18:33:53 +0900

    abstract

---
+++ Chapter08/src/abstractex/ComputerTest.java
@@ -0,0 +1,15 @@
+package abstractex;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+		
+		Computer computer = new Desktop();
+		computer.display();
+		computer.turnOff();
+		
+		Computer myNote = new MyNoteBook();
+		myNote.display();
+	}
+
+}

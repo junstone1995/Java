f2d7835e2a8e595552fa23dba2193d7cbb8991a7
junstone1995 <boris0730@gmail.com>
2020-09-04 16:00:25 +0900

    iostream

--- Chapter14/src/stream/reader/FileReaderTest.java
+++ Chapter14/src/stream/reader/FileReaderTest.java
@@ -1,8 +1,6 @@
 package stream.reader;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
